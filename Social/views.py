e7532c8746b653f941f0a301958221e1438673c5
ScottQuinn144 <scott.quinn144@outlook.com>
2022-10-15 15:11:35 +0000

    addded profile page and styles. connected urls

--- Social/views.py
+++ Social/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Profile
 
 def dashboard(request):
     return render(request, "base.html")
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "socials/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "socials/profile.html", {"profile": profile})
